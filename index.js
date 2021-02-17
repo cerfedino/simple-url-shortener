b23a0e9cf0c29e03eb420470a5dceedaa0bd7da8
Albert Cerfeda <cerfeda.albert@gmail.com>
2021-02-17 14:21:56 +0000

    Added IP logging feature

--- index.js
+++ index.js
@@ -21,8 +21,27 @@ const updateMapping = function(){
         consoleStatus("",GREEN,"+",`Imported mapping.json`);
     });
 }
-
 updateMapping();
+
+const logRequest = function(req){
+    var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+
+    let date_ob = new Date(Date.now());
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+
+    fs.appendFile('log.csv', `${year}-${month}-${date};${hours}:${minutes}:${seconds};${ip};${req.url}\n`, function (err) {
+        if (err) throw err;
+        consoleStatus("\t",GREEN,"+",`Logged into CSV file!`);
+      });
+}
+
+
 app.listen(port, () => {
     consoleStatus("",GREEN,"+",`Server is running on port ${port}`)
     setInterval(function() {
@@ -36,6 +55,7 @@ app.get('/*', (req, res) => {
     consoleStatus("",YELLOW,".",`Received request url "${url}"`);
     if (url in mapping){
         consoleStatus("\t",GREEN,"+",`Found match for "${url}"\n\t\t Redirecting to ${mapping[url]} ...`);
+        logRequest(req);
         res.writeHead(301, {'Location' : mapping[url]});
     }else{
         consoleStatus("\t",RED,"-",`Did not find any matches for "${url}"`);
@@ -50,4 +70,5 @@ const YELLOW = "\x1b[33m";
 const consoleStatus = function(prepend,COLOR,symbol,text){
     console.log(`${prepend}[${COLOR}${symbol}`+'\x1b[0m]'+` ${text}`);
 }
+
 
