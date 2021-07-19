43aca22951dc47d50511f8507207c2bd70fbe7c9
AlbertCerfeda <cerfeda.albert@gmail.com>
2021-07-19 18:58:02 +0200

    Cleaned up the code a little

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-const express = require('express')
 var fs = require('fs');
-const app = express()
-const port = process.env.PORT || 80
-var mapping = {}
+const app = require('express')();
+const port = process.env.PORT || 80;
 
-const updateMapping = function(){
-    fs.readFile('mapping.json', 'utf8', function (err, data) {
+var mapping = {};
+
+
+{
+    updateMapping();
+    
+    app.listen(port, () => {
+        logToConsole.GREEN(0,"+",`Server is running on port ${port}`);
+        setInterval(updateMapping, 5*60*1000)
+    });
+
+
+    app.get('/*', (req, res) => {
+        res.set('Cache-Control', 'no-store')
+
+        url = req.url.substr(1);
+        logToConsole.YELLOW(0,".",`Received request url "${url}"`);
+
+        if (url in mapping){
+            logToConsole.GREEN(1,"+",`Found match for "${url}"\n\t\t Redirecting to ${mapping[url]} ...`);
+            logRequest(req);
+            res.writeHead(301, {'Location' : mapping[url]});
+        }else{
+            logToConsole.RED(1,"-",`Did not find any matches for "${url}"`);
+        }
+
+        res.end();
+    });
+}
+
+
+
+function updateMapping() {
+    fs.readFile('mapping.json', 'utf8', (err, data) => {
         if (err) throw err;
 
         json = JSON.parse(data);
 
         newMapping = {};
-        json.forEach(function(json_map){
-            json_map['shortUrls'].forEach(function (shortUrl){
+        json.forEach( (json_map) => {
+            json_map['shortUrls'].forEach( (shortUrl) => {
                 newMapping[shortUrl] = json_map['longUrl'];
             });
         });
         mapping = newMapping;
 
-        consoleStatus("",GREEN,"+",`Imported mapping.json`);
+        logToConsole.GREEN(0,"+",`Imported mapping.json`);
     });
-}
-updateMapping();
+};
 
-const logRequest = function(req){
+
+function logRequest(req) {
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
 
     let date_ob = new Date(Date.now());
@@ -35,40 +65,25 @@ const logRequest = function(req){
     let minutes = date_ob.getMinutes();
     let seconds = date_ob.getSeconds();
 
-    fs.appendFile('log.csv', `${year}-${month}-${date};${hours}:${minutes}:${seconds};${ip};${req.url}\n`, function (err) {
+    fs.appendFile('log.csv', `${year}-${month}-${date};${hours}:${minutes}:${seconds};${ip};${req.url}\n`, (err) => {
         if (err) throw err;
-        consoleStatus("\t",GREEN,"+",`Logged into CSV file!`);
-      });
-}
+        logToConsole.GREEN(1,"+",`Logged into CSV file!`);
+    });
+};
 
 
-app.listen(port, () => {
-    consoleStatus("",GREEN,"+",`Server is running on port ${port}`)
-    setInterval(function() {
-        updateMapping();
-    }, 5 * 60 * 1000)
-})
+var logToConsole = (indent_lvl,COLOR,symbol,text) => {
+    console.log(`${"\t".repeat(indent_lvl)}[${COLOR}${symbol}`+'\x1b[0m]'+` ${text}`);
+};
 
-app.get('/*', (req, res) => {
-    res.set('Cache-Control', 'no-store')
-    url = req.url.substr(1);
-    consoleStatus("",YELLOW,".",`Received request url "${url}"`);
-    if (url in mapping){
-        consoleStatus("\t",GREEN,"+",`Found match for "${url}"\n\t\t Redirecting to ${mapping[url]} ...`);
-        logRequest(req);
-        res.writeHead(301, {'Location' : mapping[url]});
-    }else{
-        consoleStatus("\t",RED,"-",`Did not find any matches for "${url}"`);
-    }
+logToConsole.RED = (indent_lvl,symbol,text) => {
+    logToConsole(indent_lvl, "\x1b[31m",symbol,text);
+};
 
-    res.end();
-})
+logToConsole.GREEN = (indent_lvl,symbol,text) => {
+    logToConsole(indent_lvl, "\x1b[32m",symbol,text);
+};
 
-const RED = "\x1b[31m";
-const GREEN = "\x1b[32m"
-const YELLOW = "\x1b[33m";
-const consoleStatus = function(prepend,COLOR,symbol,text){
-    console.log(`${prepend}[${COLOR}${symbol}`+'\x1b[0m]'+` ${text}`);
-}
-
-
+logToConsole.YELLOW = (indent_lvl,symbol,text) => {
+    logToConsole(indent_lvl, "\x1b[33m",symbol,text);
+};
